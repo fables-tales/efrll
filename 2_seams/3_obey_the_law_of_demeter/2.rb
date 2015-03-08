9c6479c57f2ce48ed0b0f74a563e8055f42c9a95
Sam Phippen <samphippen@googlemail.com>
2015-03-08 12:49:41 +0000

    Mapping and selecting example demeter

---
+++ 2_seams/3_obey_the_law_of_demeter/2.rb
@@ -0,0 +1,24 @@
+items = [1,2,3,4,5]
+
+
+class MapsAndSelects
+  def initialize(value)
+    @value = value
+  end
+
+  def mapped_value
+    ((value+1)*20).to_s
+  end
+
+  def will_be_selected
+    (value+1).even? && mapped_value.length < 3
+  end
+
+  protected
+
+  attr_reader :value
+end
+
+#debateable?
+p items.map { |item| item + 1 }.select {|item| item.even? }.map { |x| x * 20 }.map(&:to_s).select {|x| x.length < 3 }
+p items.map { |item| MapsAndSelects.new(item) }.select {|x| x.will_be_selected}.map{|x| x.mapped_value}

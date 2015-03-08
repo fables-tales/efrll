5ec566544b1af9e89926e08c3591a7ca60e05907
Sam Phippen <samphippen@googlemail.com>
2015-03-08 12:56:41 +0000

    Add threequal examples

---
+++ 2_seams/6_triple_equals/2.rb
@@ -0,0 +1,25 @@
+class EmailMatcher
+  def ===(other)
+    other.split("@").count === 2 &&
+      other.split("@").last.include?(".")
+  end
+end
+class ValidatesEmails
+  def initialize(email)
+    @email = email
+  end
+
+  def valid?
+    email_matcher === email
+  end
+
+  protected
+
+  attr_reader :email
+
+  def email_matcher
+    EmailMatcher.new
+  end
+end
+
+p ValidatesEmails.new("bob@example.co").valid?

e1988d66f678fc5ea33b3aa99e88864c2b1dd83a
Sam Phippen <samphippen@googlemail.com>
2015-05-06 14:16:40 +0100

    Proc and lambda examples

---
+++ 4_enumerable_and_callables/2_prefer_lambda_instead_of_proc/1_proc.rb
@@ -0,0 +1,37 @@
+p = Proc.new { |arg_one, arg_two|
+  p arg_one
+  p arg_two
+}
+
+p.call(1,2,3)
+
+#Thanks to @jcoglan for the example
+
+def foo(&blk)
+  p [:foo, 1]
+  blk.call
+  p [:foo, 2]
+end
+
+def bar(&blk)
+  p [:bar, 1]
+  foo(&blk)
+  p [:bar, 2]
+end
+
+def baz
+  p [:baz, 1]
+  bar { return }
+  p [:baz, 2]
+end
+
+baz
+
+
+def foo
+  f = {}
+  p f.fetch(:foo) { |key| return }
+  p "here!"
+end
+
+foo
---
+++ 4_enumerable_and_callables/2_prefer_lambda_instead_of_proc/2_lambda.rb
@@ -0,0 +1,37 @@
+l = lambda { |arg_one, arg_two|
+  p arg_one
+  p arg_two
+}
+
+l.call(1,2)
+
+#Thanks to @jcoglan for the example
+
+def foo(&blk)
+  p [:foo, 1]
+  blk.call
+  p [:foo, 2]
+end
+
+def bar(&blk)
+  p [:bar, 1]
+  foo(&blk)
+  p [:bar, 2]
+end
+
+def baz
+  p [:baz, 1]
+  bar(&(lambda { return }))
+  p [:baz, 2]
+end
+
+baz
+
+
+def foo
+  f = {}
+  p f.fetch(:foo, &(lambda { |key| return }))
+  p "here!"
+end
+
+foo

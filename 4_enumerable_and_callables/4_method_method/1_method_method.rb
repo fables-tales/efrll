4b9001e8f631aafd6d20d33cb2c27874c741a67d
Sam Phippen <samphippen@googlemail.com>
2015-05-06 14:25:35 +0100

    Method method examples

---
+++ 4_enumerable_and_callables/4_method_method/1_method_method.rb
@@ -0,0 +1,36 @@
+class ReallyComplexStringTransform
+  def transform(string)
+    a = string.upcase
+    a += "ifojoweiqfjqwoiefjoq"
+    a[4] = "f"
+    a
+  end
+end
+
+
+strings = ["single", "page", "web", "apps", "don't", "work"]
+
+t = ReallyComplexStringTransform.new.method(:transform)
+strings.map(&t).each do |new_string|
+  p new_string
+end
+
+
+#HAZARD
+#
+class MyHttpLibrary
+  def http_method
+    "GET"
+  end
+
+  def foo
+
+  end
+end
+
+m = MyHttpLibrary.new.method(:foo)
+
+t1 = ReallyComplexStringTransform.new.method(:transform).call("String")
+t2 = ReallyComplexStringTransform.new.send(:transform, "String")
+
+p t1 == t2
